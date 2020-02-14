10f68f082c70e82581821ac881242f9e5bd818a9
marcelogere <marcelogere@users.noreply.github.com>
2020-02-14 20:49:59 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,11 +5,20 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 # ...
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
+
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
