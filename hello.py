f1aa66e6dbd9ad0e0fe1db3c3dfb91448710267c
marcelogere <marcelogere@users.noreply.github.com>
2020-02-17 17:39:21 -0300

    Update hello.py
    @app.shell_context_processor

--- hello.py
+++ hello.py
@@ -45,6 +45,10 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
